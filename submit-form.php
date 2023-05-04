5117797a4f44c9371bfa75140fa7c361aa246902
Nicolas Defée <nicolas.defee@gmx.de>
2023-05-04 22:24:49 +0200

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -21,10 +21,10 @@
       $mail->isSMTP();                                              // Send using SMTP
       $mail->Host       = 'smtp.gmail.com';                         // Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                     // Enable SMTP authentication
-      $mail->Username   = 'nicdef.develop@gmail.com';               // SMTP username
-      $mail->Password   = 'btaeoznxncgbcojr';                       // SMTP password
+      $mail->Username   = '';                                       // SMTP username
+      $mail->Password   = '';                                       // SMTP password
       $mail->SMTPSecure = 'tls';                                    // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-      $mail->Port       = '587';                                    // TCP port to connect to
+      $mail->Port       = '';                                       // TCP port to connect to
 
       // Recipients
       $mail->setFrom($email, $name);
